14c660e27f95f5ed1030c6aadb6c2bc4779e83f2
Ilan <ilan.malka26@gmail.com>
2015-02-23 22:41:42 +0100

    arranger mainactivity

--- app/src/main/java/com/remindus/projetcommun/remindus/MainActivity.java
+++ app/src/main/java/com/remindus/projetcommun/remindus/MainActivity.java
@@ -36,7 +36,7 @@ public class MainActivity extends ActionBarActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
 
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
