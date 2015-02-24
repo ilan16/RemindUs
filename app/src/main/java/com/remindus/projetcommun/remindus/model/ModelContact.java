ad91b66d79760034ec831e0db8b6dd03763825c7
Ilan <ilan.malka26@gmail.com>
2015-02-24 17:28:35 +0100

    ajout bdd contact pour test et modele contact

---
+++ app/src/main/java/com/remindus/projetcommun/remindus/model/ModelContact.java
@@ -0,0 +1,35 @@
+package com.remindus.projetcommun.remindus.model;
+
+/**
+ * Created by Ilan on 24/02/2015.
+ */
+public class ModelContact {
+
+    private long id;
+    private String contact;
+    private String telephone;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone;
+    }
+}
