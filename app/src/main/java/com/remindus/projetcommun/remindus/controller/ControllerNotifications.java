9ce9473541191aa9b64873e598c5058d3ff47eb2
samairi <samir.amairi.19@gmail.com>
2015-03-06 15:24:19 +0100

    Ajout du menu notifications avec fonctions vibreur,silencieux,normal

--- app/src/main/java/com/remindus/projetcommun/remindus/controller/ControllerNotifications.java
+++ app/src/main/java/com/remindus/projetcommun/remindus/controller/ControllerNotifications.java
@@ -1,9 +1,15 @@
 package com.remindus.projetcommun.remindus.controller;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
 
 import com.remindus.projetcommun.remindus.R;
 
@@ -15,38 +21,94 @@ import java.util.Locale;
 
 public class ControllerNotifications extends ActionBarActivity {
 
-    private Locale myLocale;
+    private CheckBox normal, silencieux, vibreur;
+    private Button button;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_afficher_notifications);
 
+        addListenerOnChkNormal();
+        addListenerOnButton();
+    }
+
+    public void addListenerOnChkNormal() {
+
+        normal = (CheckBox) findViewById(R.id.notif_normal);
+        normal.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (((CheckBox) v).isChecked()) {
+
+                    AudioManager audiomanage = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                    audiomanage.setRingerMode(AudioManager.RINGER_MODE_NORMAL);
+
+                    Toast.makeText(ControllerNotifications.this, "Mode normal activé !", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        silencieux = (CheckBox) findViewById(R.id.notif_silencieux);
+        silencieux.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (((CheckBox) v).isChecked()) {
+
+                    AudioManager audiomanage = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                    audiomanage.setRingerMode(AudioManager.RINGER_MODE_SILENT);
+
+                    Toast.makeText(ControllerNotifications.this, "Mode silencieux activé !", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+        vibreur = (CheckBox) findViewById(R.id.notif_vibreur);
+        vibreur.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                if (((CheckBox) v).isChecked()) {
+
+                    AudioManager audiomanage = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+                    audiomanage.setRingerMode(AudioManager.RINGER_MODE_VIBRATE);
+
+                    Toast.makeText(ControllerNotifications.this, "Mode vibreur activé !", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.global, menu);
-        return true;
+    public void addListenerOnButton() {
+
+        silencieux = (CheckBox) findViewById(R.id.notif_silencieux);
+        vibreur = (CheckBox) findViewById(R.id.notif_vibreur);
+        normal = (CheckBox) findViewById(R.id.notif_normal);
+        button = (Button) findViewById(R.id.notif_button);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                StringBuffer result = new StringBuffer();
+                result.append("Silencieux check : ").append(silencieux.isChecked());
+                result.append("\nVibreur check : ").append(vibreur.isChecked());
+                result.append("\nNormal check :").append(normal.isChecked());
+
+                Toast.makeText(ControllerNotifications.this, result.toString(), Toast.LENGTH_LONG).show();
+
+            }
+        });
+
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-
 }
