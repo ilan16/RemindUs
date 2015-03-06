e1893fae3c7870be9557a384207723fbfc1c195d
samairi <samir.amairi.19@gmail.com>
2015-03-06 16:10:25 +0100

    modif menu controller notif

--- app/src/main/java/com/remindus/projetcommun/remindus/controller/ControllerNotifications.java
+++ app/src/main/java/com/remindus/projetcommun/remindus/controller/ControllerNotifications.java
@@ -65,5 +65,12 @@ public class ControllerNotifications extends ActionBarActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.global, menu);
+        return true;
+    }
+
 
 }
