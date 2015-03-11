aecaf827284da8feccf3a2bb4567d0bc0aa425ff
samairi <samir.amairi.19@gmail.com>
2015-03-11 12:16:42 +0100

    Ajout du contactez-nous (version 70%)

--- app/src/main/java/com/remindus/projetcommun/remindus/controller/ControllerMail.java
+++ app/src/main/java/com/remindus/projetcommun/remindus/controller/ControllerMail.java
@@ -1,43 +1,45 @@
 package com.remindus.projetcommun.remindus.controller;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import com.remindus.projetcommun.remindus.R;
 
-/**
- * Created by bahia on 23/02/2015.
- */
 public class ControllerMail extends ControllerHeader {
+
+    Button btnOK;
+    EditText txtTo;
+    EditText txtSubject;
+    EditText txtMessage;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vue_afficher_contact);
+        setContentView(R.layout.vue_afficher_mail);
+        btnOK = (Button) findViewById(R.id.mail_bouton);
+        txtTo = (EditText) findViewById(R.id.mail_destinataire);
+        txtTo.requestFocus();
+        txtSubject = (EditText) findViewById(R.id.mail_destinataire);
+        txtMessage = (EditText) findViewById(R.id.mail_message);
+        btnOK.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String to = txtTo.getText().toString();
+                String subject = txtSubject.getText().toString();
+                String message = txtMessage.getText().toString();
+                Intent mail = new Intent(Intent.ACTION_SEND);
+                mail.putExtra(Intent.EXTRA_EMAIL, new String[]{to});
+                mail.putExtra(Intent.EXTRA_SUBJECT, subject);
+                mail.putExtra(Intent.EXTRA_TEXT, message);
+                mail.setType("message/rfc822");
+                startActivity(Intent.createChooser(mail, "Send email via:"));
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.global, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // TODO Auto-generated method stub
-        switch (item.getItemId()) {
-            case R.id.bouton_parametre:
-                Intent intent = new Intent(ControllerMail.this, ControllerParametre.class);
-                startActivity(intent);
-                break;
-
-        }
-
-        return false;
-    }
 
 }
