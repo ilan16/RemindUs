e34db162abdbbba90f60ba96255721cdb84d0229
kevin <kvnsadoun@gmail.com>
2015-03-25 17:51:08 +0100

    commenataire declencheurmode service

--- app/src/main/java/com/remindus/projetcommun/remindus/Service/DeclencheurModeService.java
+++ app/src/main/java/com/remindus/projetcommun/remindus/Service/DeclencheurModeService.java
@@ -16,13 +16,19 @@ import com.remindus.projetcommun.remindus.model.ModelRDV;
  * Created by kevin on 25/03/2015.
  */
 public class DeclencheurModeService extends Service {
-
+    /**
+     *
+     */
     @Override
     public void onCreate() {
 
     }
 
-
+    /**
+     *
+     * @param intent
+     * @return
+     */
     @Override
     public IBinder onBind(Intent intent) {
         // TODO Auto-generated method stub
@@ -30,6 +36,9 @@ public class DeclencheurModeService extends Service {
         return null;
     }
 
+    /**
+     *
+     */
     @Override
     public void onDestroy() {
         // detruire le service
@@ -37,20 +46,24 @@ public class DeclencheurModeService extends Service {
 
     }
 
+    /**
+     *
+     * @param intent
+     * @param startId
+     */
     @Override
     public void onStart(Intent intent, int startId) {
         //connection à la base de donnée
         DAORDV daordv = new DAORDV(this);
         ModelRDV modelRDV = daordv.prochainRDV(0);
-        //recupeation de la date et heure du prochain envoi
+        //recupeation de la date et heure du prochain RDV
         long date = modelRDV.getDatedebut();
-        Log.i("test1",date+"");
         if(date!=0) {
             //si il existe un prochain message programmé
-            //on se redirige vers SmsService.class
+            //on se redirige vers Mode.class
             Intent myIntent = new Intent(DeclencheurModeService.this, Mode.class);
             PendingIntent pendingIntent = PendingIntent.getService(DeclencheurModeService.this, 0, myIntent, 0);
-            //on redemarre un nouveau service DeclencheurSms pour le prochaoin sms
+            //on redemarre un nouveau service DeclencheurModeService pour le prochaoin RDV
             Intent myIntent2 = new Intent(getApplicationContext(), DeclencheurModeService.class);
             PendingIntent pendingIntent2 = PendingIntent.getService(getApplicationContext(), 0, myIntent2, 0);
 
@@ -60,16 +73,21 @@ public class DeclencheurModeService extends Service {
 
             //verifie quand demarré le nouveau service declencheurSms
             alarmManager.set(AlarmManager.RTC_WAKEUP, date, pendingIntent2);
-            //on ferme le service smsService pour evité qu'il renvoi un nouveau message aléatoirement
+            //on ferme le service Mode pour evité qu'il ne se déclenche aléatoirement
             DeclencheurModeService.this.stopService(myIntent);
             //on detruit se service
             this.stopSelf() ;
         }else{
-            //si il a plus de prochain message programmé on detruit se service
+            //si il a plus de prochain RDV on detruit se service
             this.stopSelf();
         }
     }
 
+    /**
+     *
+     * @param intent
+     * @return
+     */
     @Override
     public boolean onUnbind(Intent intent) {
         // TODO Auto-generated method stub
